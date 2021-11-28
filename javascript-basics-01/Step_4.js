426ac5a472039ca0309966aab516119c3888d80a
Zakaria <your.zakaria_bashir98@hotmail.com>
2021-11-28 14:13:03 +0200

    Step_4.js

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,8 @@
+function myfunction(){
+var name=document.getElementById(`name`).value;
+var surname=document.getElementById(`surname`).value;
+var city=document.getElementById(`city`).value;
+alert(`name:${name}
+surname:${surname}
+city:${city}
+`);}
