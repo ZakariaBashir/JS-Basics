2686816e098ca424369cf4a1cb9b68c867a8cda3
Zakaria <your.zakaria_bashir98@hotmail.com>
2021-11-28 16:38:56 +0200

    Step_8.js

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,9 @@
+function calculate() {
+    var yourage= document.getElementById(`age`).value;
+
+    if (yourage >= 18){
+        alert ("You are over 18!")
+    } else {
+        alert ("You are under 18!")
+    }
+}
